0bc3940dc5fd77f4aff43df5023682acf6d6cc2d
tYohannes <teklegy@gmail.com>
2022-02-21 22:26:43 +0300

    Add removeBook action react-redux binding with the delete-book button

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,11 +1,18 @@
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
-const BooksLit = () => (
-  <div className="book-list">
-    <h2>BooksList</h2>
-    <Book />
-    <Book />
-  </div>
-);
+const BooksLit = () => {
+  const books = useSelector(state => state.booksReducer)
+  console.log(books);
+
+  return (
+    <div className="book-list">
+      <h2>BooksList</h2>
+      {
+        books.map(book => <Book key={book.id} book={book} />)
+      }
+    </div>
+  )
+};
 
 export default BooksLit;

c33b46385d19ce3237af49fb195c28da948c350b
tYohannes <teklegy@gmail.com>
2022-02-21 22:26:05 +0300

    Add addBook action react-redux binding with the add-book button

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,11 +1,34 @@
-const AddBookForm = () => (
-  <form action="" className="form-add-book">
-    <input type="text" name="bookTitle" id="book-title" placeholder="title" required />
-    <select name="category" id="category">
-      <option value="" disabled selected>Category</option>
-    </select>
-    <input type="submit" value="Add Book" />
-  </form>
-);
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+
+import {addBook} from '../redux/books/books';
+
+const AddBookForm = () => {
+  const dispatch = useDispatch();
+  
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+
+    const {title, author} = e.target.elements;
+
+    const newBook = {
+      id: uuidv4(),
+      title: title.value,
+      author: author.value,
+    }
+    e.target.reset();
+    title.focus = true;
+
+    dispatch(addBook(newBook));
+  }
+
+  return (
+    <form onSubmit={submitBookToStore} className="form-add-book">
+      <input type="text" name="title" id="book-title" placeholder="title" required />
+      <input type="text" name="author" id="book-author" placeholder='author' required />
+      <input type="submit" value="Add Book" />
+    </form>
+  )
+};
 
 export default AddBookForm;

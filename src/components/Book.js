d22176b250788732c74a12dcd2cc7c9daf38e664
tYohannes <teklegy@gmail.com>
2022-02-23 23:19:56 +0300

    Add styling to book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,12 @@
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 import { deleteBook } from '../redux/books/books';
 
 const Book = ({ book }) => {
+  const progressValue = 64;
   const dispatch = useDispatch();
 
   const deleteBookFromStore = (e) => {
@@ -14,20 +17,30 @@ const Book = ({ book }) => {
   return (
     <div className="book">
       <div className="book-detail">
-        <p>{book.category}</p>
-        <h3>{book.title}</h3>
-        <p>{book.author || 'John Doe'}</p>
-        <button type="button">Comments</button>
-        <button type="button" id={book.id} onClick={deleteBookFromStore}>Remove</button>
-        <button type="button">Edit</button>
+        <p className="book-category">{book.category}</p>
+        <p className="book-title">{book.title}</p>
+        <p className="book-author">{book.author || 'John Doe'}</p>
+        <button className="btn-comments" type="button">Comments</button>
+        <button className="btn-remove" type="button" id={book.id} onClick={deleteBookFromStore}>Remove</button>
+        <button className="btn-edit" type="button">Edit</button>
       </div>
       <div className="progress-container">
-        <p>64% completed</p>
+        <div style={{ width: 50, height: 50 }} className="circle-wrap">
+          <CircularProgressbar value={progressValue} />
+        </div>
+        <div className="progress-text">
+          <p className="progress-percentage">
+            {progressValue}
+            %
+          </p>
+          <p className="progress-completed">completed</p>
+        </div>
       </div>
-      <div className="current-chapter">
-        <p>CURRENT CHAPTER</p>
-        <p>Chapter 15</p>
-        <button type="button">Update Progress</button>
+      <hr className="vertical-separator" />
+      <div className="current-chapter-container">
+        <p className="current-chapter">CURRENT CHAPTER</p>
+        <p className="current-lesson">Lesson</p>
+        <button className="btn-update-progress" type="button">Update Progress</button>
       </div>
     </div>
   );
